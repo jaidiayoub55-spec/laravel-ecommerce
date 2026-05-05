59176eb32ab887897e553bfa291c065037941511
jaidiayoub55-spec <jaidiayoub55@gmail.com>
2026-05-05 22:17:45 +0100

    Fix typo in timestamp method for wishlists table

--- database/migrations/2026_04_15_164417_create_wishlists_table.php
+++ database/migrations/2026_04_15_164417_create_wishlists_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('product_id')->constrained()->cascadeOnDelete();
-            $table->timestemp();
+            $table->timestemps();
         });
     }
 
